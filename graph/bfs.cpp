70e48548ec5668ea53d8356d447b6765298cd7b7
Tanav Poswal <tanavposwal939@gmail.com>
2024-11-05 13:59:19 +0530

    dfs-bfs

--- graph/bfs.cpp
+++ graph/bfs.cpp
@@ -3,37 +3,45 @@
 #include <queue>
 using namespace std;
 
-class Solution {
-  public:
-    // Function to return Breadth First Traversal of given graph.
-    vector<int> bfsOfGraph(int V, vector<int> adj[]) {
-        int vis[V] = {0}; 
-        vis[0] = 1; 
-        queue<int> q;
-        // push the initial starting node 
-        q.push(0); 
-        vector<int> bfs; 
-        // iterate till the queue is empty 
-        while(!q.empty()) {
-           // get the topmost element in the queue 
-            int node = q.front(); 
-            q.pop(); 
-            bfs.push_back(node); 
-            // traverse for all its neighbours 
-            for(auto it : adj[node]) {
-                // if the neighbour has previously not been visited, 
-                // store in Q and mark as visited 
-                if(!vis[it]) {
-                    vis[it] = 1; 
-                    q.push(it); 
-                }
+
+vector<int> bfsOfGraph(int V, vector<int> adj[]) {
+    int vis[V] = {0}; 
+    vis[0] = 1; 
+    queue<int> q;
+    // push the initial starting node 
+    q.push(0); 
+    vector<int> bfs; 
+    // iterate till the queue is empty 
+    while(!q.empty()) {
+        // get the topmost element in the queue 
+        int node = q.front(); 
+        q.pop(); 
+        bfs.push_back(node); 
+        // traverse for all its neighbours 
+        for(auto it : adj[node]) { 
+            // if the neighbour has previously not been visited, 
+            // store in Q and mark as visited 
+            if(!vis[it]) {
+                vis[it] = 1; 
+                q.push(it); 
             }
         }
-        return bfs; 
     }
-};
+    return bfs; 
+}
 
 int main(){
-    cout << "hi";
+
+    int V = 7;
+    vector<int> adj[] = {{3, 2}, {2, 4, 3}, {0, 1, 3}, {0, 2, 4}, {1, 3, 5}, {4, 6}, {5}};
+;
+
+    vector<int> res = bfsOfGraph(V, adj);
+
+    for (int i: res) {
+        cout << i << " ";
+    }
+    cout << endl;
+
     return 0;
 }
--- graph/dfs.cpp
+++ graph/dfs.cpp
@@ -2,31 +2,36 @@
 #include <vector>
 using namespace std;
 
-class Solution {
-  private:
-    void dfs(int node, vector<int> adj[], int vis[], vector<int> &ls) {
-        vis[node] = 1;
-        ls.push_back(node);
-        
-        for(auto it:adj[node]){
-            if (!vis[it]) {
-                dfs(it, adj, vis, ls);
-            }
+void dfs(int node, vector<int> adj[], int vis[], vector<int> &ls) {
+    vis[node] = 1;
+    ls.push_back(node);
+    
+    for(auto it:adj[node]){
+        if (!vis[it]) {
+            dfs(it, adj, vis, ls);
         }
     }
-  public:
-    // Function to return a list containing the DFS traversal of the graph.
-    vector<int> dfsOfGraph(int V, vector<int> adj[]) {
-        int vis[V] = {0};
-        int start = 0;
-        vector<int> ls;
-        dfs(start, adj, vis, ls);
-        return ls;
-    }
-};
+}
+vector<int> dfsOfGraph(int V, vector<int> adj[]) {
+    int vis[V] = {0};
+    int start = 0;
+    vector<int> ls;
+    dfs(start, adj, vis, ls);
+    return ls;
+}
 
 
 int main(){
-    cout << "hi";
+    int V = 7;
+    vector<int> adj[] = {{3, 2}, {2, 4, 3}, {0, 1, 3}, {0, 2, 4}, {1, 3, 5}, {4, 6}, {5}};
+;
+
+    vector<int> res = dfsOfGraph(V, adj);
+
+    for (int i: res) {
+        cout << i << " ";
+    }
+    cout << endl;
+
     return 0;
 }
